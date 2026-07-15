df4c804963c04895f286106570367460ef3ff039
Nikhil2706-hue <2706nikhil@gmail.com>
2026-07-15 19:28:43 +0530

    Add files via upload

---
+++ 14-7-2026_BITWISE op.py
@@ -0,0 +1,104 @@
+'''
+Task:1 
+Complete the following table:
+
+Decimal| Binary | ~ (NOT) | << 1  |  << 2  |  >> 1  |  >> 2  |
+5        0101       -6       10       20         2       1     
+8        1000       -9       16       32         4       2      
+10       1010       -11      20       40         5       2    
+12       1100       -13      24       48         6       3     
+15       1111       -16      30       60         7       3  
+20       10100      -21      40       80        10       5    
+25       11001      -26      50       100       12       6   
+32       100000     -32      64       128       16       8  
+'''
+
+
+#Task:2
+#Assignment – Bitwise Operators (Python)
+
+a = 10
+b = 6
+print(a & b)   #output: 2
+
+
+c = 12
+d = 5
+print(c | d)    #output: 13
+
+
+
+m = 15
+n = 9
+print(m ^ n)   #output: 6
+
+
+
+p = 20
+print(~p)   #output:  -21
+
+
+q = 8
+print(q << 1)   #output: 16
+
+
+
+r = 8
+print(r << 3)  #output: 64
+
+
+
+s = 64
+print(s >> 2)  #output: 16
+
+
+
+u = 25
+print(u >> 3)  #output: 3
+
+
+
+x = 10
+y = 5
+print(x & y)  #output: 0
+print(x | y)   #output: 15
+print(x ^ y)  #output: 15
+
+
+
+z = 7
+print(~z)        #output: -8
+print(z << 2)  #output: 28
+print(z >> 1)  #output: 3
+
+'''
+Also write the binary representation of the following numbers:
+* 5    -    0101
+* 9    -    1001
+* 12  -    1100
+* 18  -   10010
+* 25  -   11001
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
